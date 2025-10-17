823740968374552341ae70fb23301fabb64b6f1e
Veshal Raj <veshal.raj@fractalphive.com>
2025-10-17 11:18:15 +0530

    inistal project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import CommonRouter from "./routes/common"
+import './App.css'
+function App() {
+
+
+  return (
+    <>
+
+     <CommonRouter/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/common/UserSwitchPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function UserSwitchPage() {
+  return (
+    <div className='w-screen h-screen overflow-hidden flex justify-center items-center'>
+        <div className='w-[1000px] h-[100px] bg-amber-600'>
+
+        </div>
+    </div>
+  )
+}
